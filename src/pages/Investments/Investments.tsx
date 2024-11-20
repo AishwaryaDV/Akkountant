6df5d2c58937d2274a66d848c40150d37d31f33f
AishwaryaDV <millamatew@gmail.com>
2024-11-21 00:28:33 +0530

    feat: sectors and watchlist updated

--- src/pages/Investments/Investments.tsx
+++ src/pages/Investments/Investments.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import "../Investments/Investments.scss";
 import { FaEye } from "react-icons/fa";
+import { MdUpdate } from "react-icons/md";
+import { IoMdAddCircleOutline } from "react-icons/io";
 
 const Investments = () => {
   return (
@@ -24,39 +26,98 @@ const Investments = () => {
             </div>
             <div className="investmentOption">
                 <div className="investmentIcon"><FaEye size={35}/></div>
-                <div className="investmentType">Stocks</div>
+                <div className="investmentType">Gold</div>
             </div>
             <div className="investmentOption">
                 <div className="investmentIcon"><FaEye size={35}/></div>
-                <div className="investmentType">Stocks</div>
+                <div className="investmentType">Mutual Fund</div>
             </div>
             <div className="investmentOption">
                 <div className="investmentIcon"><FaEye size={35}/></div>
-                <div className="investmentType">Stocks</div>
+                <div className="investmentType">NPS</div>
             </div>
             <div className="investmentOption">
                 <div className="investmentIcon"><FaEye size={35}/></div>
-                <div className="investmentType">Stocks</div>
+                <div className="investmentType">PPF</div>
             </div>
             <div className="investmentOption">
                 <div className="investmentIcon"><FaEye size={35}/></div>
-                <div className="investmentType">Stocks</div>
+                <div className="investmentType">EPF</div>
             </div>
             <div className="investmentOption">
                 <div className="investmentIcon"><FaEye size={35}/></div>
-                <div className="investmentType">Stocks</div>
+                <div className="investmentType">FD</div>
             </div>
             
         </div>
         <div className="watchlistHeader">Watchlist</div>
+        <span className="addWatchlistItem"><IoMdAddCircleOutline style={{verticalAlign:"middle"}} size={35}/></span>
         <div className="watchlistContainer">
-            <div className="watchlistOption">Stocks</div>
-            <div className="watchlistOption">Stocks</div>
-            <div className="watchlistOption">Stocks</div>
-            <div className="watchlistOption">Stocks</div>
-            <div className="watchlistOption">Stocks</div>
-            <div className="watchlistOption">Stocks</div>
-            <div className="watchlistOption">Stocks</div>
+            <div className="watchlistOption">
+                <div className="watchlistIconSection">1</div>
+                <div className="watchlistInvestmentNameSection">
+                    <div className="watchlistInvestmentName">Stocks</div>
+                    <div className="lastUpadtedValue"><MdUpdate size={30} style={{verticalAlign:"middle"}} />: 8:42 AM </div>
+
+                </div>
+                <div className="watchlistInvestmentValueSection">3</div>
+            </div>
+
+            <div className="watchlistOption">
+                <div className="watchlistIconSection">1</div>
+                <div className="watchlistInvestmentNameSection">
+                    <div className="watchlistInvestmentName">Gold</div>
+                    <div className="lastUpadtedValue"><MdUpdate size={30} style={{verticalAlign:"middle"}} />: 8:42 AM </div>
+
+                </div>
+                <div className="watchlistInvestmentValueSection">3</div>
+            </div>
+            <div className="watchlistOption">
+                <div className="watchlistIconSection">1</div>
+                <div className="watchlistInvestmentNameSection">
+                    <div className="watchlistInvestmentName">Mutual Funds</div>
+                    <div className="lastUpadtedValue"><MdUpdate size={30} style={{verticalAlign:"middle"}} />: 8:42 AM </div>
+
+                </div>
+                <div className="watchlistInvestmentValueSection">3</div>
+            </div>
+            <div className="watchlistOption">
+                <div className="watchlistIconSection">1</div>
+                <div className="watchlistInvestmentNameSection">
+                    <div className="watchlistInvestmentName">National Pension Scheme</div>
+                    <div className="lastUpadtedValue"><MdUpdate size={30} style={{verticalAlign:"middle"}} />: 8:42 AM </div>
+
+                </div>
+                <div className="watchlistInvestmentValueSection">3</div>
+            </div>
+            <div className="watchlistOption">
+                <div className="watchlistIconSection">1</div>
+                <div className="watchlistInvestmentNameSection">
+                    <div className="watchlistInvestmentName">Public Provident Fund</div>
+                    <div className="lastUpadtedValue"><MdUpdate size={30} style={{verticalAlign:"middle"}} />: 8:42 AM </div>
+
+                </div>
+                <div className="watchlistInvestmentValueSection">3</div>
+            </div>
+            <div className="watchlistOption">
+                <div className="watchlistIconSection">1</div>
+                <div className="watchlistInvestmentNameSection">
+                    <div className="watchlistInvestmentName">Employee Provident Fund</div>
+                    <div className="lastUpadtedValue"><MdUpdate size={30} style={{verticalAlign:"middle"}} />: 8:42 AM </div>
+
+                </div>
+                <div className="watchlistInvestmentValueSection">3</div>
+            </div>
+
+            <div className="watchlistOption">
+                <div className="watchlistIconSection">1</div>
+                <div className="watchlistInvestmentNameSection">
+                    <div className="watchlistInvestmentName">Fixed Deposits</div>
+                    <div className="lastUpadtedValue"><MdUpdate size={30} style={{verticalAlign:"middle"}} />: 8:42 AM </div>
+
+                </div>
+                <div className="watchlistInvestmentValueSection">3</div>
+            </div>
         </div>
     </div>
   )
